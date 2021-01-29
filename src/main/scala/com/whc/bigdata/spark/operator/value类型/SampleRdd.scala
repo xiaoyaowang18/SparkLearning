4ad76ba365dafedcf005ce9737153a4ea5fcee4b
= <=>
2021-01-29 18:01:32 +0800

    'key-value类型'

--- src/main/scala/com/whc/bigdata/spark/operator/value类型/SampleRdd.scala
+++ src/main/scala/com/whc/bigdata/spark/operator/value类型/SampleRdd.scala
@@ -15,7 +15,7 @@ object SampleRdd {
 
     //以指定的随机种子随机抽样出数量为fraction的数据，withReplacement表示是抽出的数据是否放回，true为有放回的抽样，false为无放回的抽样，seed用于指定随机数生成器种子。
     //对数据源进行多次抽样，可以查看哪个数据出现的次数多，可能造成数据倾斜。
-    val samplerdd: RDD[Int] = listrdd.sample(true,1,1)
+    val samplerdd: RDD[Int] = listrdd.sample(true,2,2)
 
     samplerdd.collect().foreach(println)
   }
